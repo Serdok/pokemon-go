5d9537a6177ccd1013ecf63ea2a288ef066e2a04
Anass 'Serdok' Lahnin <l.anass.pro@gmail.com>
2022-02-06 22:49:23 +0100

    API Core: Create openapi.yaml from template
    We create the specification from a template to allow placeholder values. For now, this is only used to define the server url to customize it if launching it from a dev server (i.e. localhost)

---
+++ scripts/openapi_config.py
@@ -0,0 +1,24 @@
+import oyaml as yaml
+import string
+
+
+placeholder = 'server_endpoint'
+yaml_signal = 'py'
+template = string.Template('{{$signal:$placeholder}}')
+
+
+def process_openapi_template_file(app_engine_url):
+    with open('./api/openapi.template.yaml', 'r', encoding='utf-8') as stream:
+        try:
+            spec = yaml.safe_load(stream)
+            for server in spec['servers']:
+                if server['url'] == template.substitute(signal=yaml_signal, placeholder=placeholder):
+                    server['url'] = f'{app_engine_url}/api/v1'
+        except yaml.YAMLError as err:
+            print(err)
+
+    with open('./api/openapi.yaml', 'w', encoding='utf-8') as stream:
+        try:
+            yaml.safe_dump(spec, stream, default_flow_style=False, allow_unicode=True)
+        except yaml.YAMLError as err:
+            print(err)
