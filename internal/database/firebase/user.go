42299b2d23d88b2c1381184e41325e546a317378
Anass 'Serdok' Lahnin <l.anass.pro@gmail.com>
2022-02-12 22:32:17 +0100

    API: Implemented user creation and retrieval

--- internal/database/firebase/user.go
+++ internal/database/firebase/user.go
@@ -1,18 +1,45 @@
 package firebase
 
-import "github.com/Serdok/pokemon-go/internal/models"
+import (
+	"context"
+	"github.com/Serdok/pokemon-go/internal/models"
+	"log"
+)
 
-func (fs *FireStore) Create(user *models.User) (*models.User, error) {
+func (fs *FireStore) Create(ctx context.Context, user *models.User) (created *models.User, err error) {
+	created = &models.User{}
 
-	return nil, nil
+	// Reference to document
+	ref := fs.db.Collection("users").Doc(user.Uid)
+
+	// Using Create to disallow overwriting of existing users
+	_, err = ref.Create(ctx, user)
+	if err != nil {
+		return nil, err
+	}
+
+	snap, err := ref.Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+	err = snap.DataTo(created)
+	return created, err
 }
 
-func (fs *FireStore) GetByUid(uid string) (*models.User, error) {
+func (fs *FireStore) GetByUid(ctx context.Context, uid string) (user *models.User, err error) {
+	user = &models.User{}
 
-	return nil, nil
+	snap, err := fs.db.Collection("users").Doc(uid).Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println(snap.Data())
+	err = snap.DataTo(user)
+	return user, err
 }
 
-func (fs *FireStore) Update(user *models.User) (*models.User, error) {
+func (fs *FireStore) Update(ctx context.Context, user *models.User) (*models.User, error) {
 
 	return nil, nil
 }
