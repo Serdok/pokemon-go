d00afb8e26a6b9289feb0b599034e84c24b5c7dd
Anass 'Serdok' Lahnin <l.anass.pro@gmail.com>
2022-02-20 20:24:34 +0100

    Renamed interface functions to avoid conflicts with other views

--- internal/database/firebase/user.go
+++ internal/database/firebase/user.go
@@ -18,8 +18,8 @@ func (fb Firebase) VerifyToken(ctx context.Context, token string) error {
 	return nil
 }
 
-// Get a user document from its uid
-func (fb Firebase) Get(ctx context.Context, uid string) (*models.User, error) {
+// GetUser a user document from its uid
+func (fb Firebase) GetUser(ctx context.Context, uid string) (*models.User, error) {
 	ref := fb.fs.Collection("users").Doc(uid)
 	snap, err := ref.Get(ctx)
 	if err != nil {
@@ -34,19 +34,19 @@ func (fb Firebase) Get(ctx context.Context, uid string) (*models.User, error) {
 	return user, nil
 }
 
-// Create a user from given data. Uid must be unique
-func (fb Firebase) Create(ctx context.Context, user models.User) (*models.User, error) {
+// CreateUser a user from given data. Uid must be unique
+func (fb Firebase) CreateUser(ctx context.Context, user models.User) (*models.User, error) {
 	ref := fb.fs.Collection("users").Doc(user.Uid)
 
 	_, err := ref.Create(ctx, user)
 	if err != nil {
 		return nil, err
 	}
-	return fb.Get(ctx, user.Uid)
+	return fb.GetUser(ctx, user.Uid)
 }
 
-// Delete a user from its uid
-func (fb Firebase) Delete(ctx context.Context, uid string) error {
+// DeleteUser a user from its uid
+func (fb Firebase) DeleteUser(ctx context.Context, uid string) error {
 	ref := fb.fs.Collection("users").Doc(uid)
 	_, err := ref.Delete(ctx)
 	return err
